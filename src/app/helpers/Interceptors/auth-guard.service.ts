4c4868ced054e84d53aec5dc24024f82472d6a45
Surendra <surendra.battula@archents.com>
2023-05-15 16:40:57 +0530

    chore: updating angular

--- src/app/helpers/Interceptors/auth-guard.service.ts
+++ src/app/helpers/Interceptors/auth-guard.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { AuthenticationService } from '../../services/authentication.service';
 
 @Injectable({
     providedIn: 'root'
 })
 
-export class AuthGuardService implements CanActivate {
+export class AuthGuardService  {
 
     constructor(private router: Router, private authService: AuthenticationService) { }
 
